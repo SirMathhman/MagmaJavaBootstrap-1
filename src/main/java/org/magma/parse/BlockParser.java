de7e62db73893a2958df544d9bc5f880884df2a3
SirMathhman <sirmathhman@gmail.com>
2020-06-13 16:40:07 -0400

    Add BlockParser

---
+++ src/main/java/org/magma/parse/BlockParser.java
@@ -0,0 +1,31 @@
+package org.magma.parse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import org.magma.Compiler;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+public class BlockParser extends JSONUnit implements Parser {
+	protected BlockParser(ObjectMapper mapper) {
+		super(mapper);
+	}
+
+	@Override
+	public Optional<JsonNode> parse(String content, Compiler compiler) {
+		if (content.startsWith("{") && content.endsWith("}")) {
+			String children = content.substring(1, content.length() - 1);
+			ArrayNode items = createArray();
+			Arrays.stream(children.split(";"))
+					.filter(s -> !s.isBlank())
+					.map(String::trim)
+					.map(compiler::parse)
+					.forEach(items::add);
+			return Optional.of(createObject()
+					.set("children", items));
+		}
+		return Optional.empty();
+	}
+}

b1cb731d636c50c940933f1b7c7eabc4d5c62b80
SirMathhman <sirmathhman@gmail.com>
2020-06-13 17:14:00 -0400

    Add VoidNameResolver

--- src/main/java/org/magma/name/VoidNameResolver.java
+++ src/main/java/org/magma/name/VoidNameResolver.java
@@ -2,21 +2,19 @@ package org.magma.name;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.inject.Inject;
 import org.magma.Compiler;
 
 import java.util.Optional;
 
-public class IntNameResolver extends AbstractNameResolver {
-	@Inject
-	protected IntNameResolver(ObjectMapper mapper) {
+public class VoidNameResolver extends AbstractNameResolver {
+	public VoidNameResolver(ObjectMapper mapper) {
 		super(mapper);
 	}
 
 	@Override
 	public Optional<JsonNode> resolveName(String name, Compiler compiler) {
 		return Optional.of(name)
-				.filter("Int"::equals)
+				.filter("Void"::equals)
 				.map(this::wrapInNode);
 	}
 }

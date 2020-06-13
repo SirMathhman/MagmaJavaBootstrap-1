6a72af690be1b4e6cf956260b11f6d0f3df430a1
SirMathhman <sirmathhman@gmail.com>
2020-06-13 14:17:43 -0400

    Add DoubleParser

---
+++ src/main/java/org/magma/parse/DoubleParser.java
@@ -0,0 +1,52 @@
+package org.magma.parse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.inject.Inject;
+
+import java.util.Optional;
+
+/**
+ * Parsers doubles using {@link Double#parseDouble(String)}.
+ * The content is truncated if it ends with the character "d".
+ * If content similar to "10.0", "5.2", "3.87" -- doesn't end with an "f",
+ * then that content is assumed to be valid as a float and not a double.
+ */
+public class DoubleParser extends JSONParser {
+	@Inject
+	public DoubleParser(ObjectMapper mapper) {
+		super(mapper);
+	}
+
+	/**
+	 * Attempts to parse the content using {@link Double#parseDouble(String)},
+	 * and returns an empty result if a {@link NumberFormatException} was thrown.
+	 *
+	 * @param content <p>The content that is to be parsed.
+	 *                Should already be trimmed using {@link String#trim()}, and
+	 *                should not be null.
+	 *                </p>
+	 * @return The wrapped double.
+	 */
+	@Override
+	public Optional<JsonNode> parse(String content) {
+		if (isLackingIdentifier(content)) return Optional.empty();
+		try {
+			String formatted = content.substring(0, content.length() - 1);
+			double result = Double.parseDouble(formatted);
+			return buildNode(result);
+		} catch (NumberFormatException e) {
+			return Optional.empty();
+		}
+	}
+
+	private static boolean isLackingIdentifier(String content) {
+		return content.isEmpty() || 'd' != content.charAt(content.length() - 1);
+	}
+
+	private Optional<JsonNode> buildNode(double result) {
+		return Optional.of(createObject()
+				.put("type", "double")
+				.put("value", result));
+	}
+}

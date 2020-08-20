6544d46d24ef81d78f6b80f7091c7dc00ddb7202
SirMathhman <sirmathhman@gmail.com>
2020-08-20 17:23:25 -0400

    Remove bad code

--- src/com/meti/Config.java
+++
@@ -1,7 +0,0 @@
-package com.meti;
-
-public interface Config {
-	void load() throws LoggedException;
-
-	void store() throws LoggedException;
-}
--- src/com/meti/LoggedException.java
+++
@@ -1,7 +0,0 @@
-package com.meti;
-
-public class LoggedException extends Exception {
-	public LoggedException(String message, Throwable cause) {
-		super(message, cause);
-	}
-}

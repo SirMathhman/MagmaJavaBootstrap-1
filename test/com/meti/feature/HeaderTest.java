0198b9a27721d73894711d202ec186aa079a49ea
SirMathhman <sirmathhman@gmail.com>
2020-08-27 17:33:43 -0400

    Add HeaderTest

---
+++ test/com/meti/feature/HeaderTest.java
@@ -0,0 +1,29 @@
+package com.meti.feature;
+
+public class HeaderTest extends FeatureTest {
+	@Override
+	protected int expectedExit() {
+		return 0;
+	}
+
+	@Override
+	protected String source() {
+		return """
+				import native stdio;
+								
+				def main() : Int => {
+					return 0;
+				}
+				""";
+	}
+
+	@Override
+	protected String expectedOutput() {
+		return "";
+	}
+
+	@Override
+	protected String compile() {
+		return "#include <stdio.h>\nint main(){return 0;}";
+	}
+}

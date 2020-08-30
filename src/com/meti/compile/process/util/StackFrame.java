63daef01ca53a25391964c0a803a7c769afb4731
SirMathhman <sirmathhman@gmail.com>
2020-08-30 17:31:55 -0400

    Add InvalidNameTest

--- src/com/meti/compile/process/util/StackFrame.java
+++ src/com/meti/compile/process/util/StackFrame.java
@@ -6,40 +6,64 @@ import java.util.*;
 import java.util.function.Function;
 
 public class StackFrame {
-	private final Map<String, Declaration> definitions = new HashMap<>();
-	private final List<String> generics = new ArrayList<>();
+    private final Map<String, Declaration> definitions = new HashMap<>();
+    private final List<String> generics = new ArrayList<>();
 
-	public String define(String name, Type type) {
-		if (!definitions.containsKey(name)) {
-			definitions.put(name, new Declaration(name));
-		}
-		return definitions.get(name).define(type);
-	}
+    public String define(String name, Type type) {
+        if (name.isBlank()) {
+            throw new IllegalArgumentException("A variable can't be blank.");
+        }
+        if (!definitions.containsKey(name)) {
+            char first = name.charAt(0);
+            boolean isValid = Character.isUpperCase(first)
+                    || Character.isLowerCase(first)
+                    || first == '_';
+            String after = name.substring(1);
+            for (char c : after.toCharArray()) {
+                boolean isValidCharacter = (Character.isUpperCase(c)
+                        || Character.isLowerCase(c)
+                        || Character.isDigit(c)
+                        || c == '_') && (c != ' ' && c != ',');
+                if (!isValidCharacter) {
+                    isValid = false;
+                }
+            }
+            //TODO: Check for C keywords
+            String nameToUse;
+            if (isValid) {
+                nameToUse = name;
+            } else {
+                nameToUse = "__%d__".formatted(name.hashCode());
+            }
+            definitions.put(name, new Declaration(nameToUse));
+        }
+        return definitions.get(name).define(type);
+    }
 
-	public boolean isDefined(String name) {
-		return definitions.containsKey(name);
-	}
+    public boolean isDefined(String name) {
+        return definitions.containsKey(name);
+    }
 
-	public List<Type> lookup(String name) {
-		return definitions.get(name).listTypes();
-	}
+    public List<Type> lookup(String name) {
+        return definitions.get(name).listTypes();
+    }
 
-	@Override
-	public String toString() {
-		return definitions.keySet().toString();
-	}
+    @Override
+    public String toString() {
+        return definitions.keySet().toString();
+    }
 
-	public <T> Optional<T> lookup(String name, Function<Type, T> function) {
-		return Optional.of(name)
-				.filter(definitions::containsKey)
-				.map(definitions::get)
-				.map(definition -> definition.applyFirst(function));
-	}
+    public <T> Optional<T> lookup(String name, Function<Type, T> function) {
+        return Optional.of(name)
+                .filter(definitions::containsKey)
+                .map(definitions::get)
+                .map(definition -> definition.applyFirst(function));
+    }
 
-	public Optional<String> lookup(String name, Type type) {
-		return Optional.of(name)
-				.filter(definitions::containsKey)
-				.map(definitions::get)
-				.map(definition -> definition.lookup(type));
-	}
+    public Optional<String> lookup(String name, Type type) {
+        return Optional.of(name)
+                .filter(definitions::containsKey)
+                .map(definitions::get)
+                .map(definition -> definition.lookup(type));
+    }
 }

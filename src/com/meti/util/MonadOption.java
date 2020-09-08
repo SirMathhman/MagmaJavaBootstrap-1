5c0670970a8fe303f133b2330e0e32a4b085726f
SirMathhman <sirmathhman@gmail.com>
2020-09-08 11:27:47 -0400

    Add more transformative method in MonadOption

--- src/com/meti/util/MonadOption.java
+++ src/com/meti/util/MonadOption.java
@@ -1,6 +1,7 @@
 package com.meti.util;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public interface MonadOption<T> {
     <R, E extends Throwable> R applyOrThrow(Function<T, R> function, E exception) throws E;
@@ -8,4 +9,8 @@ public interface MonadOption<T> {
     <R> DuadOption<T, R> with(R other);
 
     MonadStream<T> stream();
+
+    MonadOption<T> filter(Predicate<T> predicate);
+
+    <R> MonadOption<R> map(Function<T, R> function);
 }

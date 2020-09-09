a1bfb0c260cac62c6b4307b6f1405e4515eb7c49
SirMathhman <sirmathhman@gmail.com>
2020-09-09 12:35:24 -0400

    Add MagmaLexer to isolate MagmaCompiler

--- src/com/meti/compile/MagmaCompiler.java
+++ src/com/meti/compile/MagmaCompiler.java
@@ -1,28 +1,21 @@
 package com.meti.compile;
 
-import com.meti.compile.lex.Lexer;
-import com.meti.compile.lex.RootLexer;
-import com.meti.compile.lex.parse.MagmaTokenizerFactory;
-import com.meti.compile.lex.parse.TokenizerFactory;
-import com.meti.compile.lex.resolve.MagmaResolveRule;
-import com.meti.compile.lex.resolve.ResolveRule;
 import com.meti.compile.node.Token;
 import com.meti.compile.process.FixProcessStage;
 import com.meti.compile.process.ProcessStage;
 import com.meti.compile.process.TypeProcessStage;
-import com.meti.util.Monad;
+
+import static com.meti.util.Monad.Monad;
 
 public class MagmaCompiler implements Compiler {
-    private final TokenizerFactory rootParserRule = new MagmaTokenizerFactory();
-    private final ResolveRule rootResolveRule = new MagmaResolveRule();
-    private final Lexer lexer = new RootLexer(rootParserRule, rootResolveRule);
     private final ProcessStage typeStage = new TypeProcessStage();
     private final ProcessStage fixStage = new FixProcessStage();
+    private final MagmaLexer magmaLexer = new MagmaLexer();
 
     @Override
     public String compile(String value) {
         return "{%s}".formatted(value)
-                .transform(lexer::parseToOption)
+                .transform(magmaLexer::parseToOption)
                 .applyOrThrow(this::applyPipeline, createInvalidTree(value));
     }
 
@@ -33,7 +26,7 @@ public class MagmaCompiler implements Compiler {
     }
 
     private String applyPipeline(Token token) {
-        return Monad.Monad(token)
+        return Monad(token)
                 .map(fixStage::process)
                 .map(typeStage::process)
                 .map(Token::render)
---
+++ src/com/meti/compile/MagmaLexer.java
@@ -0,0 +1,34 @@
+package com.meti.compile;
+
+import com.meti.compile.lex.Lexer;
+import com.meti.compile.lex.RootLexer;
+import com.meti.compile.lex.parse.MagmaTokenizerFactory;
+import com.meti.compile.lex.parse.TokenizerFactory;
+import com.meti.compile.lex.resolve.MagmaResolveRule;
+import com.meti.compile.lex.resolve.ResolveRule;
+import com.meti.compile.node.Token;
+import com.meti.compile.type.Type;
+import com.meti.util.Option;
+
+public class MagmaLexer implements Lexer {
+    private final TokenizerFactory rootParserRule = new MagmaTokenizerFactory();
+    private final ResolveRule rootResolveRule = new MagmaResolveRule();
+    private final Lexer lexer = new RootLexer(rootParserRule, rootResolveRule);
+
+    public Option<Token> parseToOption(String content) {
+        return lexer.parseToOption(content);
+    }
+
+    @Override
+    public Token parse(String content) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Type resolve(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    public MagmaLexer() {
+    }
+}
